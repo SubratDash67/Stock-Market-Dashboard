08b0d5999024119a9733765f157992bb0e2a5645
SubratDash67 <subratdash2022@gmail.com>
2024-12-19 20:35:09 +0530

    Automated sync commit

--- sync.bat
+++ sync.bat
@@ -1,6 +1,7 @@
 @echo off
 cd "C:\Users\KIIT\Desktop\Stock-Market-Dashboard"
-git pull origin main
+git fetch origin main
+git merge origin/main
 git add .
 git commit -m "Automated sync commit"
 git push origin main
